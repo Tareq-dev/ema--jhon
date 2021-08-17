2aaa2d8b54a1b01cc3e5cc30c167436384d9ded0
unknown <tareque179@gmail.com>
2021-08-17 12:59:13 +0600

    shop page coding done

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Product.css';
+
 const Product = (props) => {
     const { img, name, seller, price, stock } = props.product;
     return (
         <div className="product">
             <div>
-                <img src={img} alt="" />
+            <img src={img} alt="" />
             </div>
             <div>
             <h4 className="product-name">{name}</h4>
-            <p><small>by:{seller}</small></p>
-            <p><small>${price}</small></p>
+            <p><small>by: {seller}</small></p>
+            <p><small>$ {price}</small></p>
             <p><small>Only {stock} left in stock - Order soon</small></p>
-            <button className="main-button"> add to cart</button>
+            <button
+             className="main-button" 
+             onClick = {() =>props.handleAddProduct(props.product)}
+             > add to cart</button>
             </div>
-            
         </div>
     );
 };
